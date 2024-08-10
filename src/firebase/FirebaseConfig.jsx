014261b181386805e1cea6df0d46371b9de12619
kaushalgujrathi <144025827+kaushalgujrathi@users.noreply.github.com>
2024-08-10 13:08:25 +0530

    Update FirebaseConfig.jsx

--- src/firebase/FirebaseConfig.jsx
+++ src/firebase/FirebaseConfig.jsx
@@ -5,14 +5,14 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDdmoUIq3rVqEdd03JrROWQH5EiHmFKgf8",
-  authDomain: "store-c8656.firebaseapp.com",
-  projectId: "store-c8656",
-  storageBucket: "store-c8656.appspot.com",
+  apiKey: "apikey",
+  authDomain: ".firebaseapp.com",
+  projectId: "projectId",
+  storageBucket: ".appspot.com",
 
   
-  messagingSenderId: "890555851619",
-  appId: "1:890555851619:web:a480e5834ee83fb5b5e956"
+  messagingSenderId: "msgid",
+  appId: "appid"
 };
 
 // Initialize Firebase
